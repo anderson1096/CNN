6d0584c269ee5e6669f43651a7363cd710468bd9
Anderson <anderson.mosquera9610@gmail.com>
2018-06-04 10:12:23 -0500

    Add files

---
+++ prueba.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import ImageOps
+import torch.optim as optim
+from PIL import Image
+import matplotlib.pyplot as plt
+import time
+import segmentation
+
+idx = ['0','1','2','3','4','5','6','7','8','9',
+       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def pil_loader(path):
+	with open(path, 'rb') as f:
+		img = Image.open(f)
+		return img.convert('L')
+
+trans = transforms.Compose([transforms.Resize((32,32)), transforms.ToTensor()])
+mydata = dset.ImageFolder('./char74k',transform=trans, loader=pil_loader)
+loader = torch.utils.data.DataLoader(mydata, batch_size=128, shuffle=True, num_workers=2)
+#print (mydata.classes)
+#print (idx)
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(1,32, kernel_size=5)
+		self.conv2 = nn.Conv2d(32,128, kernel_size=5)
+		self.conv2_drop = nn.Dropout2d()
+		self.fc1 = nn.Linear(3200, 2048)
+		self.fc3 = nn.Linear(2048, 512)
+		self.fc5 = nn.Linear(512, 47)
+
+	def forward(self, x):
+		x = F.relu(F.max_pool2d(self.conv1(x),2))
+		x =F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+		x = x.view(-1, 3200)
+		x = F.relu(self.fc1(x))
+		x = F.dropout(x, training=self.training)
+		x = F.relu(self.fc3(x))
+		x = F.dropout(x, training=self.training)
+		x = self.fc5(x)
+		return x
+
+model = Net()
+optimizer = optim.Adam(model.parameters(), lr=1e-4, eps=1e-4)
+
+def train(epoch):
+	model.train()
+	for batch_idx, (data, target) in enumerate(loader):
+		data, target = Variable(data), Variable(target)
+		optimizer.zero_grad()
+		output = model(data)
+		loss = nn.CrossEntropyLoss()
+		output = loss(output, target)
+		output.backward()
+		optimizer.step()
+		if batch_idx % 100 == 0:
+			print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                 epoch, batch_idx * len(data), len(loader.dataset),
+                 100. * batch_idx / len(loader), output.data.item()))
+
+'''for epoch in range(5):
+	train(epoch)'''
+
+model = Net()
+model.load_state_dict(torch.load("char_recognizer.pt"))
+#print (model)
+
+def predict_char(gray):
+    w = gray.size[0]
+    h = gray.size[1]
+    gray = gray.convert('L')
+    gray = gray.point(lambda x: 0 if x<180 else 255, '1')
+    x= int(16- (w/2))
+    y = int(16- (h/2))
+    canvas = Image.new('L', (32, 32), (255))
+    canvas.paste(gray, box=(x, y))
+
+    #canvas = ImageOps.invert(canvas)
+    canvas = np.array(canvas)
+    #canvas = canvas / 255.0
+    
+    #test_data = np.array(gray)
+    test_output = model(Variable(torch.FloatTensor(canvas).unsqueeze(0).unsqueeze(0).data))
+    #print (test_output.data.max(1, keepdim=True))
+    pred = test_output.data.max(1, keepdim=True)[1] 
+    pred = np.array(pred).squeeze(0).squeeze(0)
+    #print('El caracter es =>', idx[pred])
+    #plt.imshow(canvas)
+    #plt.show()
+    return idx[pred]
+
+res = ""
+for i in range(6):
+	pil_im = Image.open("./Test/test{}.jpg".format(i))
+	res += predict_char(pil_im)
+print ('La placa es:  ', res)
+#torch.save(model.state_dict(), 'char_recognizer.pt')
+
---
+++ segmentation.py
@@ -0,0 +1,65 @@
+import cv2
+import imutils
+import operator
+
+def digit_segmentation(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	thresh = cv2.threshold(gray, 85, 240, cv2.THRESH_BINARY)[1]
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+	digits = {}
+	for c in cnts:
+		(x, y, w, h) = cv2.boundingRect(c)
+		if (w > 90 and w < 300) and (h > 90 and h < 300):
+			cv2.rectangle(image, (x - 10, y - 10), (x + w + 10, y + h + 10), (255, 0, 0), 5)
+			roi = thresh[y : y + h, x : x + w]
+			digits[x] = roi
+
+	return digits
+
+def save_digits(images):
+	for idx, image in enumerate(images):
+		image = cv2.resize(image, (28, 32), interpolation = cv2.INTER_CUBIC)
+		image = roi_center(image)
+		cv2.imwrite('./Test/test{}.jpg'.format(idx), image)
+
+
+def roi_center(roi):
+	ALTURA_ROI = 28
+	ANCHURA_ROI = 32
+	ANCHO_BORDE = 4
+
+	relacion_aspecto_roi = float(roi.shape[1]) / float(roi.shape[0])
+	nueva_anchura = int((ALTURA_ROI * relacion_aspecto_roi) + 0.5)
+	b_top = ANCHO_BORDE
+	b_bottom = ANCHO_BORDE
+	b_left = int((ANCHURA_ROI - nueva_anchura) / 2)
+	b_right = int((ANCHURA_ROI- nueva_anchura) / 2)
+
+	roi_borde = cv2.copyMakeBorder(roi,b_top,b_bottom,b_left,b_right,cv2.BORDER_CONSTANT,value=[255,255,255])
+	roi_trans = cv2.resize(roi_borde,(32,32))
+	return roi_trans
+
+
+if __name__ == "__main__":
+	image_src = cv2.imread("./Placas/placa4.jpg")
+	image_src = imutils.resize(image_src, height=500)
+	result = digit_segmentation(image_src)
+	result_sort = sorted(result.items(), key=operator.itemgetter(0))
+	final_result = []
+	
+	'''for r in result_sort:
+		print r[0]
+		final_result.append(r[1])
+
+
+	
+	for r in final_result:
+		#r = cv2.resize(r, (32, 32), interpolation = cv2.INTER_CUBIC)
+		cv2.imshow("Inicial", r)
+		
+		cv2.waitKey(0)'''
+
+	save_digits(final_result)
+	#cv2.imshow("Inicial", image_src)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
